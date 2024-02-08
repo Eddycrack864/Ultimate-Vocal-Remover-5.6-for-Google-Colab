3a05a43d730c15f6e6800b54af3ab75e1bd72f6b
Eddycrack 864 <89285504+Eddycrack864@users.noreply.github.com>
2024-02-08 17:53:32 -0500

    Update webUI.py

--- webUI.py
+++ webUI.py
@@ -224,7 +224,6 @@ model_dict = webui.models_url
 
 import os
 import wget
-from IPython.display import clear_output
 
 for category, models in model_dict.items():
     if category in ['VR Arc', 'MDX-Net']:
@@ -233,12 +232,10 @@ for category, models in model_dict.items():
         elif category == 'MDX-Net':
             model_path = 'models/MDX_Net_Models'
         for model_name, model_url in models.items():
-            cmd = f"aria2c --optimize-concurrent-downloads --console-log-level=error --summary-interval=10 -j5 -x16 -s16 -k1M -c -d {model_path} -Z {model_url}"
+            cmd = f"aria2c --optimize-concurrent-downloads --summary-interval=10 -j5 -x16 -s16 -k1M -c -q -d {model_path} -Z {model_url}"
             os.system(cmd)
-            clear_output()
         print("Models downloaded successfully.")
 
 print("Starting WebUI...")
-
 webui = UVRWebUI(uvr, online_data_path='models/download_checks.json')
 webui.launch(share=True)
