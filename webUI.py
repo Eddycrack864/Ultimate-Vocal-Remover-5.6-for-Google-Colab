e2fb89db1cf9ccf10ff5216e67559dd576833d39
Eddycrack 864 <89285504+Eddycrack864@users.noreply.github.com>
2024-02-08 19:31:05 -0500

    title update

--- webUI.py
+++ webUI.py
@@ -143,9 +143,9 @@ class UVRWebUI:
     def define_layout(self):
         with gr.Blocks() as app:
             self.app = app
-            gr.HTML("<h1> 🎵 UVR 5.6 🎵 </h1>")
-            gr.Markdown("## Space created by [Not Eddy (Spanish Mod)](http://discord.com/users/274566299349155851) in [AI HUB](https://discord.gg/aihub). Credits to r3gm for making part of the GUI.")
-            gr.Markdown("## You can use this into a Hugging Face Space [here](https://huggingface.co/spaces/Eddycrack864/UVR5). If you liked the space you can give it a 💖 and star my repo on [GitHub](https://github.com/Eddycrack864/UVR5-5.6-for-Colab).")
+            gr.HTML("<h1> 🎵 Ultimate Vocal Remover 5.6 for Google Colab 🎵 </h1>")
+            gr.Markdown("## Colab created by [Not Eddy (Spanish Mod)](http://discord.com/users/274566299349155851) in [AI HUB](https://discord.gg/aihub) server.")
+            gr.Markdown("## You can also use this into a Hugging Face Space [here](https://huggingface.co/spaces/Eddycrack864/UVR5). If you liked the space and colab you can give it a 💖 and star my repo on [GitHub](https://github.com/Eddycrack864/UVR5-5.6-for-Colab).")
             with gr.Tabs():
                 with gr.TabItem("Process"):
                     with gr.Row():
